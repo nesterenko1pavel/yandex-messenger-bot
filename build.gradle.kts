ac8bc6490afa279b593915a011c9c5439ec67626
Pavel <>
2024-09-15 16:02:24 +0700

    refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
-
-val kotlin_version: String by project
-
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "1.9.21"
+    id("maven-publish")
 }
+
+group = "honey.bot.api"
+group = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -14,4 +15,12 @@ dependencies {
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
 }
